21513cf88cab5c3d556baa3d7841528927247251
Nguyen Nam <nguyenphuongnam.ptit@gmail.com>
2018-01-17 15:45:20 +0700

    abc
    xyz

---
+++ app/src/main/java/com/example/quochung/brainwars2/SignUp.java
@@ -0,0 +1,36 @@
+package com.example.quochung.brainwars2;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class SignUp extends AppCompatActivity {
+    MaterialEditText edtPhone, edtUsername, edtPassword;
+    Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        edtPhone = (MaterialEditText) findViewById(R.id.edtPhone);
+        edtUsername = (MaterialEditText) findViewById(R.id.edtUsername);
+        edtPassword = (MaterialEditText) findViewById(R.id.edtPassword);
+
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
+                mDialog.setMessage("Please waiting...");
+                mDialog.show();
+            }
+        });
+    }
+
+}
+
